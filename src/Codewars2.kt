9bb6f86c4453571a4d148ea2cbb356bb501839b5
twinown <niyaz9797@gmail.com>
2023-01-27 22:41:05 +0100

    2

--- src/Codewars2.kt
+++
@@ -1,2 +0,0 @@
-class Codewars2 {
-}
---
+++ src/Codewars_2_rabota_s_list_any_summa_chisel.kt
@@ -0,0 +1,15 @@
+/*Given an array of integers as strings and numbers,
+return the sum of the array values as if all were
+numbers.
+Return your answer as a number.*/
+object Codewars_2_rabota_s_list_any_summa_chisel {
+    @JvmStatic
+    fun sum(mixed: List<Any>): Int = mixed.sumOf { it.toString().toInt() }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(sum(listOf(5, "10")))
+    }
+}
+
+
