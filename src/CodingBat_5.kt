fd5e6c041fc37c823c2dfb0e1a3ebba99fe99199
twinown <niyaz9797@gmail.com>
2023-02-02 11:42:48 +0300

    5

--- src/CodingBat_5.kt
+++ src/CodingBat_5.kt
@@ -1,2 +1,13 @@
-class CodingBat_5 {
+import kotlin.math.abs
+
+object CodingBat_5 {
+    fun makeNegative(x: Int) = if (x < 0) x else x * (-1)
+
+    //easier way
+ //   fun makeNegative(x:Int) = -abs(x)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(makeNegative(0))
+    }
 }
