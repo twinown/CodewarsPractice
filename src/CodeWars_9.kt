2e07564e7e4720e1b3984e87092210fb3eda2680
twinown <niyaz9797@gmail.com>
2023-02-07 12:01:31 +0300

    9

---
+++ src/CodeWars_9.kt
@@ -0,0 +1,11 @@
+/*Clock shows h hours, m minutes and s seconds after midnight.
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+h = 0
+m = 1
+s = 1*/
+
+/*result = 61000*/
+class CodeWars_9 {
+    fun mS (h:Int, m:Int, s:Int) = ((3600*h) + (60 * m) + s)*1000
+}
