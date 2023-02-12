3974d81a55a5120875469a9548e3baa303395a9f
twinown <niyaz9797@gmail.com>
2023-02-12 23:10:33 +0300

    13_round_to_integer_exmp_75.35_or_75.55_to_75

---
+++ src/CodingWars_13_round_to_integer.kt
@@ -0,0 +1,14 @@
+import kotlin.math.sqrt
+
+object CodingWars_13_round_to_integer {
+    //(exmp_75.35_or_75.55_to_75)
+    fun predictAge(age1: Int, age2: Int, age3: Int,
+                   age4: Int, age5: Int, age6: Int, age7: Int, age8: Int)=
+        (sqrt((age1 * age1 + age2 * age2 + age3 * age3 + age4 * age4 + age5 * age5 + age6 * age6 +
+                age7 * age7 + age8 * age8).toDouble())/2).toInt()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(predictAge(65, 60, 75, 55, 60, 63, 64, 45))
+    }
+}
