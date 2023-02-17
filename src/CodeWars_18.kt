9b19cda31ed3f3ca0416b762cfd0013956dd62a6
twinown <niyaz9797@gmail.com>
2023-02-17 18:06:12 +0300

    18

---
+++ src/CodeWars_18.kt
@@ -0,0 +1,8 @@
+class CodeWars_18 {
+    public  fun century(number: Int)= if(number%100==0) number/100 else number/100+1
+}
+
+fun main() {
+    val  cd:CodeWars_18 = CodeWars_18()
+    println(cd.century(1954))
+}
