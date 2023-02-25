a5437ba7289f332b4ffe645dc0f099350fe348b2
twinown <niyaz9797@gmail.com>
2023-02-25 13:41:50 +0300

    24

--- src/CodeWars_24.kt
+++ src/CodeWars_24.kt
@@ -1,17 +1,24 @@
+/*This program tests the life of an evaporator containing a gas.
+We know the content of the evaporator (content in ml), the percentage of foam or
+gas lost every day (evap_per_day) and the threshold (threshold) in percentage
+beyond which the evaporator is no longer useful. All numbers are strictly positive.
+The program reports the nth day (as an integer) on which the evaporator will be out of use.
+Example:
+evaporator(10, 10, 5) -> 29
+*/
 object CodeWars_24 {
     fun evaporator(content: Double, evap_per_day: Double, threshold: Double): Int {
-        // TODO: 24.02.2023 think of
         var day = 0
-        var left:Double  = content - (evap_per_day/100.0)
-        while (left >= threshold/100.0 ){
+        var tot  = 100.0
+        while (tot > threshold){
             day++
-            left = (left - (evap_per_day/100.0))
+            tot -= (evap_per_day / 100.0) * tot
         }
         return day
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println(evaporator(10.0,10.0,5.0))
+        println(evaporator(100.0,5.0,5.0))
     }
 }
