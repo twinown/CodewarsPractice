08d0cb882364e93532495b11622d04869275a05e
twinown <niyaz9797@gmail.com>
2023-09-12 23:13:46 +0300

    63

---
+++ src/CodeWars_63.java
@@ -0,0 +1,15 @@
+public class CodeWars_63 {
+    public static int toBinary(int n) {
+        int multiplier = 1;
+        int value = 0;
+
+        do {
+            value += (n % 2) * multiplier;
+            n /= 2;
+            multiplier *= 10;
+        } while (n > 0);
+
+        return value;
+    }
+
+}
