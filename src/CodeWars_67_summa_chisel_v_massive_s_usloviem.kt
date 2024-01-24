b598038897c873f17c0d39b0d4a06155b0b01d41
twinown <niyaz9797@gmail.com>
2024-01-24 17:24:48 +0300

    CodeWars_67_68

---
+++ src/CodeWars_67_summa_chisel_v_massive_s_usloviem.kt
@@ -0,0 +1,15 @@
+class CodeWars_67_summa_chisel_v_massive_s_usloviem {
+    fun sum(numbers: IntArray) = numbers.sumOf { if (it>=0) it
+    else 0}
+
+    //можно так еще
+    //тут внимательно на цикл for !!!!!!!!
+    fun summ (numbers: IntArray): Int {
+        var sum = 0
+        for (number in numbers){
+            if (number>=0)
+               sum +=number
+        }
+        return if (sum<0) 0 else sum
+    }
+}
---
+++ src/CodeWars_68.java
@@ -0,0 +1,3 @@
+public class CodeWars_68 {
+
+}
