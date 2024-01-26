fe32cf519b6299239e9a276dac469e2c49762a26
twinown <niyaz9797@gmail.com>
2024-01-26 16:12:43 +0300

    CodeWars_69_70

---
+++ src/CodeWars_69_ebanutii_kotlin_rabota_s_List_Any_.kt
@@ -0,0 +1,9 @@
+class CodeWars_69_ebanutii_kotlin_rabota_s_List<Any>{
+
+
+    fun filterList(l: List<Any>) = l.filterIsInstance<Int>()
+
+
+//в котле чтоб узнать является ли числом,интом и тд
+    //надо писать 'numb is Int'
+}
---
+++ src/CodeWars_70.java
@@ -0,0 +1,7 @@
+public class CodeWars_70 {
+
+    public static void main(String[] args) {
+        char ss = 'd';
+        boolean bb = Character.isDigit(ss);
+    }
+}
