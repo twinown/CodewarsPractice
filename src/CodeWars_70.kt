0f966d798e6db792204995340e8581a944a1057a
twinown <niyaz9797@gmail.com>
2024-01-28 21:11:34 +0300

    CodeWars_70_71

---
+++ src/CodeWars_70.kt
@@ -0,0 +1,3 @@
+class CodeWars_70 {
+    fun getAge(yearsOld: String)= yearsOld[0].digitToInt()
+}
---
+++ src/CodeWars_71_copyofrange_java.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class CodeWars_71_copyofrange_java {
+    public static int[] take(int[] arr, int n) {
+
+        return Arrays.copyOfRange(arr,0,Math.min(n, arr.length));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(take(new int[]{0}, 2)));
+    }
+}
