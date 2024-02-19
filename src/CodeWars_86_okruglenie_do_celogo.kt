ab732ae2229706feebebe6db16d3c514befc9ec4
twinown <niyaz9797@gmail.com>
2024-02-19 22:04:40 +0300

    CodeWars_86_87

---
+++ src/CodeWars_86_okruglenie_do_celogo.kt
@@ -0,0 +1,5 @@
+import kotlin.math.floor
+
+class CodeWars_86_okruglenie_do_celogo {
+    fun litres(time: Double):Int = floor(time*0.5).toInt()
+}
---
+++ src/CodeWars_87_obedinenie_slov_v_massive.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class CodeWars_87_obedinenie_slov_v_massive {
+    public static String smash(String... words) {
+        return Arrays.toString(words).replaceAll("[\\[\\],]","");
+        //можно и так
+       // return String.join(" ", words);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smash("dog", "cat"));
+    }
+}
