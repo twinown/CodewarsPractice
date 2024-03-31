056abea8a8436f75d932ad27a4de8569d4ecb1d1
twinown <niyaz9797@gmail.com>
2024-03-31 21:21:26 +0300

    CodeWars_116_117

---
+++ src/CodeWars_116_point_foematting_double.java
@@ -0,0 +1,8 @@
+import java.text.DecimalFormat;
+
+public class CodeWars_116_point_foematting_double {
+    public static double squareArea(double A){
+        DecimalFormat df = new DecimalFormat("#.##");
+        return Double.parseDouble(df.format(Math.pow(2*A/Math.PI,2)));
+    }
+}
---
+++ src/CodeWars_117.kt
@@ -0,0 +1,3 @@
+class CodeWars_117 {
+    fun simpleMultiplication(n: Int) = if(n%2==0) n*8 else n*9
+}
