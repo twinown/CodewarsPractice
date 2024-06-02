bb0748bff0738402245a60e6542ed42cf0be3080
twinown <niyaz9797@gmail.com>
2024-06-02 16:08:14 +0300

    162

---
+++ src/CodeWars_162.java
@@ -0,0 +1,7 @@
+public class CodeWars_162 {
+    public static String chromosomeCheck(String sperm) {
+        return sperm.equals("XX")?"Congratulations! You're going to have a daughter.":
+                "Congratulations! You're going to have a son.";
+    }
+
+}
