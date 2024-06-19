6300e76508a71744cb11dbaff677b2eaa7ff98c7
twinown <niyaz9797@gmail.com>
2024-06-19 18:04:55 +0300

    176

---
+++ src/CodeWars_176.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class CodeWars_176 {
+
+    public static int [] arrayDiff(int[] a, int[] b) {
+        List<Integer> list = new ArrayList<>();
+        int goal;
+        for (int k : a) {
+            goal = 0;
+            for (int i : b) {
+                if (k != i) {
+                    goal++;
+                }
+                if (goal == b.length) {
+                    list.add(k);
+                }
+            }
+        }
+        int [] c  = new int[list.size()];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = list.get(i);
+        }
+        return  b.length==0?a:c;
+        //or
+        //list.stream().mapToInt(i->i).toArray();
+    }
+
+    public static void main(String[] args) {
+     //   System.out.println(arrayDiff(new int[]{5, 2, 2, 2, 1}, new int[]{2}));
+        System.out.println(Arrays.toString(arrayDiff(new int[]{5, 2, 2, 2, 1}, new int[]{})));
+    }
+}
