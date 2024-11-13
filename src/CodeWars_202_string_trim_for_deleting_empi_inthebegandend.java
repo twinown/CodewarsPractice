37e509ff2211902a64b2b3e1f03810f52a3ead86
twinown <niyaz9797@gmail.com>
2024-11-13 23:38:02 +0300

    202

---
+++ src/CodeWars_202_string_trim_for_deleting_empi_inthebegandend.java
@@ -0,0 +1,5 @@
+public class CodeWars_202_string_trim_for_deleting_empi_inthebegandend {
+    public static String vaporcode(String s) {
+        return s.replace(" ","").replace("","  ").toUpperCase().trim();
+    }
+}
