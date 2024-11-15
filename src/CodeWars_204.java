1aa5b524e1ae91957361872278701b6b64437e78
twinown <niyaz9797@gmail.com>
2024-11-16 00:50:56 +0300

    204_new_fori_style

--- src/CodeWars_204.java
+++ src/CodeWars_204.java
@@ -1,2 +1,11 @@
 public class CodeWars_204 {
+    public static int cubeOdd(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            if (j % 2 != 0) {
+                sum += j * j * j;
+            }
+        }
+        return sum;
+    }
 }
