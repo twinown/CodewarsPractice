6bec6248144ac132a8e4712371024cf1b40d33de
twinown <niyaz9797@gmail.com>
2024-11-17 23:49:24 +0300

    205

---
+++ src/CodeWars_205.java
@@ -0,0 +1,9 @@
+public class CodeWars_205 {
+    public static int closeCompare(double a, double b) {
+        return 0; // TODO
+    }
+
+    public static int closeCompare(double a, double b, double margin) {
+        return 0; // TODO
+    }
+}
