7da40a61fb766a0f97470a3ef4cf0a3e8cb97f0d
twinown <niyaz9797@gmail.com>
2024-11-29 21:41:44 +0300

    214

---
+++ src/CodeWars_214.java
@@ -0,0 +1,11 @@
+public class CodeWars_214 {
+    public static String getDay(int n) {
+        return n == 1 ? "Sunday" :
+                n == 2 ? "Monday" :
+                        n == 3 ? "Tuesday" :
+                                n == 4 ? "Wednesday" :
+                                        n == 5 ? "Thursday" :
+                                                n == 6 ? "Friday" :
+                                                        n == 7 ? "Saturday" : "Wrong, please enter a number between 1 and 7";
+    }
+}
