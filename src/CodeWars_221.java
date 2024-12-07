4b7562671007ef11ed0c513c3a6b48bed71293e5
twinown <niyaz9797@gmail.com>
2024-12-07 23:19:00 +0300

    221_interesting

--- src/CodeWars_221.java
+++ src/CodeWars_221.java
@@ -1,34 +1,37 @@
 public class CodeWars_221 {
     public static String incrementString(String str) {
-        int countZeros = 0;
-        for(int i = 0;i<str.length();i++){
-            if(Character.isDigit(str.charAt(i))){
-                String buffer = str.substring(i);
-                int intBuffer;
-                if (buffer.charAt(0)=='0'){
-                    for (int j = 0; j < buffer.length(); j++) {
-                        if (buffer.charAt(j)=='0'){
-                         countZeros++;
+            String oldString = str;
+            for(int i = str.length()-1;i>=1;i--) {
+                while (Character.isDigit(str.charAt(i))){
+                    i--;
+            }
+                if (Character.isDigit(str.charAt(str.length()-1))){
+                    String buffer = str.substring(i+1);
+                    long longBuffer;
+                    if (buffer.charAt(0) == '0'){
+                        //todo попробуй если оч большое число, брать его часть последнюю, цифр 15
+                        //лонг макс ленхс
+                        if (buffer.length()>)
+                        longBuffer = Long.parseLong(buffer)+1;
+                        buffer = buffer
+                                .substring(0,buffer.lastIndexOf('0')+1)+longBuffer;
+                        str = str.substring(0,i+1)+buffer;
+                        if (str.length()>oldString.length()){
+                            str = str.replaceFirst("0","");
                         }
+                        break;
+                    } else {
+                        longBuffer = Long.parseLong(buffer)+1;
+                        buffer = String.valueOf(longBuffer);
+                        str = str.substring(0,i+1)+buffer;
+                        break;
                     }
-                    //todo попробюуй не считая нули, а сравнивая количество до и после
-                    intBuffer = Integer.parseInt(buffer)+1;
-                    buffer = buffer
-                            .substring(0,buffer.lastIndexOf('0')+1)+intBuffer;
-                    str = str.substring(0,i)+buffer;
-                    break;
-                } else {
-                    intBuffer = Integer.parseInt(buffer)+1;
-                    buffer = String.valueOf(intBuffer);
-                    str = str.substring(0,i)+buffer;
-                    break;
                 }
             }
-        }
-      return str.charAt(str.length()-1)=='0'?str.replaceFirst("0",""):str;
+        return str.isEmpty()||!Character.isDigit(str.charAt(str.length()-1))?str+"1":str;
     }
 
     public static void main(String[] args) {
-        System.out.println(incrementString("poto09"));
+        System.out.println(incrementString("w221378209659582463624230911419240671"));
     }
 }
