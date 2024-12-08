38385f49e0f2f081e0ae0c5f73aba83556aeef08
twinown <niyaz9797@gmail.com>
2024-12-08 19:32:57 +0300

    221_interesting_NO

--- src/CodeWars_221_dermo.java
+++ src/CodeWars_221_dermo.java
@@ -1,17 +1,25 @@
-public class CodeWars_221 {
+public class CodeWars_221_dermo {
     public static String incrementString(String str) {
             String oldString = str;
             for(int i = str.length()-1;i>=1;i--) {
                 while (Character.isDigit(str.charAt(i))){
                     i--;
+                    if (i==0){
+                        break;
+                    }
             }
                 if (Character.isDigit(str.charAt(str.length()-1))){
                     String buffer = str.substring(i+1);
+                    if (buffer.length()>Long.toString(Long.MAX_VALUE).length()){
+                        String secondPart = buffer.substring(buffer.length()-4);
+                        String firstPart = buffer.substring(0,buffer.length()-4);
+                        long anotherLongBuffer = Long.parseLong(secondPart)+1;
+                        buffer = firstPart+anotherLongBuffer;
+                        str = str.substring(0,i+1)+buffer;
+                        break;
+                    }
                     long longBuffer;
                     if (buffer.charAt(0) == '0'){
-                        //todo попробуй если оч большое число, брать его часть последнюю, цифр 15
-                        //лонг макс ленхс
-                        if (buffer.length()>)
                         longBuffer = Long.parseLong(buffer)+1;
                         buffer = buffer
                                 .substring(0,buffer.lastIndexOf('0')+1)+longBuffer;
@@ -32,6 +40,6 @@ public class CodeWars_221 {
     }
 
     public static void main(String[] args) {
-        System.out.println(incrementString("w221378209659582463624230911419240671"));
+        System.out.println(incrementString("vNM|IX?bCaDuHhaCrW{c!K`@*N73747152088917169657172191109187"));
     }
 }
