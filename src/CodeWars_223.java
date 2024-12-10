74b008174a80f193e92a73cee08ba18cc51633e2
twinown <niyaz9797@gmail.com>
2024-12-10 14:36:54 +0300

    223

---
+++ src/CodeWars_223.java
@@ -0,0 +1,15 @@
+public class CodeWars_223 {
+    String name = "Hero" ;
+    String position = "00";
+    int health = 100;
+    int damage = 5;
+    int experience = 0;
+
+    public CodeWars_223(){}
+
+    public CodeWars_223(String name){
+        this.name = name;
+    }
+
+}
+
