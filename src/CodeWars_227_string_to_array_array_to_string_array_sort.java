034f59614f899d9ff0877747c4ff3533c8831286
twinown <niyaz9797@gmail.com>
2024-12-14 13:51:27 +0300

    227

---
+++ src/CodeWars_227_string_to_array_array_to_string_array_sort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class CodeWars_227_string_to_array_array_to_string_array_sort {
+    public static String meeting(String s) {
+        String[] guests = s.split(";");
+        for (int i = 0; i < guests.length; i++){
+            int index = guests[i].indexOf(":");
+            guests[i] = "(" + guests[i].substring(index+1).toUpperCase() + ", " + guests[i].substring(0,index).toUpperCase() + ")";
+        }
+        Arrays.sort(guests);
+
+        return String.join("",guests);
+    }
+
+}
