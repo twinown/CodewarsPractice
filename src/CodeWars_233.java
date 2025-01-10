b67cf8e94f2574b46ea7727dd6bf50b380b91a9d
twinown <niyaz9797@gmail.com>
2025-01-10 20:26:29 +0300

    233

---
+++ src/CodeWars_233.java
@@ -0,0 +1,10 @@
+public class CodeWars_233 {
+    public static String binaryAddition(int a, int b){
+
+        return Integer.toBinaryString(a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binaryAddition(1,1));
+    }
+}
